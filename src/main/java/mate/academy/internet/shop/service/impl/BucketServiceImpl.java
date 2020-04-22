a880cc312361cfd7694eb86bc1d511763aa314ca
interami <interami@ukr.net>
2020-04-22 20:47:27 +0300

    added annotations

--- src/main/java/mate/academy/internet/shop/service/impl/BucketServiceImpl.java
+++ src/main/java/mate/academy/internet/shop/service/impl/BucketServiceImpl.java
@@ -2,12 +2,17 @@ package mate.academy.internet.shop.service.impl;
 
 import mate.academy.internet.shop.dao.BucketDao;
 import mate.academy.internet.shop.dao.ItemDao;
+import mate.academy.internet.shop.lib.Inject;
+import mate.academy.internet.shop.lib.Service;
 import mate.academy.internet.shop.model.Bucket;
 import mate.academy.internet.shop.model.Item;
 import mate.academy.internet.shop.service.BucketServuce;
 
+@Service
 public class BucketServiceImpl implements BucketServuce {
+    @Inject
     private BucketDao bucketDao;
+    @Inject
     private ItemDao itemDao;
 
     @Override
